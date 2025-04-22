5c65f0056e4f52ce13ca8bd7ca17b844f76490e1
Ayushstockmarket <161935651+Ayushstockmarket@users.noreply.github.com>
2025-04-22 20:30:31 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+document.getElementById("toggle-mode").onclick=()=>document.body.classList.toggle("dark");
+
+document.addEventListener("DOMContentLoaded", () => {
+    const detailsElements = document.querySelectorAll("details");
+
+    detailsElements.forEach(details => {
+        details.addEventListener("toggle", () => {
+            if (details.open) {
+                detailsElements.forEach(otherDetails => {
+                    if (otherDetails !== details) {
+                        otherDetails.open = false;
+                    }
+                });
+            }
+        });
+    });
+});
